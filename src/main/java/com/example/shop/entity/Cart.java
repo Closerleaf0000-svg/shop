a79cf7edc6f6b920dc247276a97df0a7df2464b6
Closerleaf0000-svg <soisgreat0000@gmail.com>
2026-09-05 23:12:14 +0800

    Create Cart.java

---
+++ src/main/java/com/example/shop/entity/Cart.java
@@ -0,0 +1,71 @@
+package com.example.shop.entity;
+
+public class Cart {
+
+    private Long cartId;
+    private Long memberId;
+    private String productId;
+    private String productName;
+    private Integer price;
+    private Integer stock;
+    private Integer quantity;
+
+    public Cart() {
+    }
+
+    public Long getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(Long cartId) {
+        this.cartId = cartId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
